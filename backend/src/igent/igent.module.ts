4725dbf084f94b8cba372df7007a86910772a90d
diegobtv <planoastartup@gmail.com>
2026-07-03 23:09:40 -0300

    Fix iGent auth module dependency

--- backend/src/igent/igent.module.ts
+++ backend/src/igent/igent.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { EntitlementsModule } from '../entitlements/entitlements.module';
 import { IGentController } from './igent.controller';
 import { IGentService } from './igent.service';
 
 @Module({
-  imports: [EntitlementsModule],
+  imports: [AuthModule, EntitlementsModule],
   controllers: [IGentController],
   providers: [IGentService],
 })
